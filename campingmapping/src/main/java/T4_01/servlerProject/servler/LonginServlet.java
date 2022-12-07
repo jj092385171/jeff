905e34ecd3b68c72908b61d524ec1d9062496853
shihshihder <suc12345@gmail.com>
2022-12-07 20:43:42 +0800

    20221209

--- campingmapping/src/main/java/T4_01/servlerProject/servler/LonginServlet.java
+++ campingmapping/src/main/java/T4_01/servlerProject/servler/LonginServlet.java
@@ -1,17 +1,11 @@
 package T4_01.servlerProject.servler;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import T4_01.servlerProject.entity.Admin;
-import T4_01.servlerProject.service.AdminService;
-import T4_01.servlerProject.service.impl.AdminServiceImpl;
 
 
 
@@ -22,36 +16,34 @@ public class LonginServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html;charset=UTF-8");
-
-//		1.拿資料
-		String UID = req.getParameter("UID");
-		String password = req.getParameter("password");
-//		2.業務邏輯
-		AdminService adminService = new AdminServiceImpl();
-		Admin admin = adminService.login(Integer.parseInt(UID), password);
-//		3.處理結果
-		PrintWriter printWriter = resp.getWriter();
-		if (admin != null) {
-//			成功
-			printWriter.println("成功");
-			
-		} else {
-//			失敗
-			printWriter.println("失敗");
-			
-		}
+		System.out.println("認證完成準備配對密碼");
+		
+//		req.setCharacterEncoding("UTF-8");
+//		resp.setContentType("text/html;charset=UTF-8");
+//
+////		1.拿資料
+//		String UID = req.getParameter("UID");
+//		String password = req.getParameter("password");
+////		2.業務邏輯
+//		AdminService adminService = new AdminServiceImpl();
+//		Admin admin = adminService.login(Integer.parseInt(UID), password);
+////		3.處理結果
+//		PrintWriter printWriter = resp.getWriter();
+//		if (admin != null) {
+////			成功
+//			printWriter.println("成功");
+//			
+//		} else {
+////			失敗
+//			printWriter.println("失敗");
+//			
+//		}
 
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("utf-8");
-		String UID = req.getParameter("UID");
-		String password = req.getParameter("password");
-		System.out.println("ulogin="+UID);
-		System.out.println("plogin="+password);
+		
 		doGet(req, resp);
 	}
 }
--- campingmapping/src/main/java/T4_01/servlet/test.java
+++ campingmapping/src/main/java/T4_01/servlet/test.java
@@ -1,18 +1,11 @@
 package T4_01.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.alibaba.fastjson.JSONObject;
 
 
 @WebServlet("/test")
@@ -22,47 +15,47 @@ public class test extends HttpServlet {
 
 	    
 	 
-	    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {}
 	 
-	            throws ServletException, IOException {
-	 
-	        response.setContentType("text/html;charset=UTF-8");
-	 
-	        //對Post中文參數進行解碼
-	 
-	        request.setCharacterEncoding("UTF-8");
-	 
-	        //取得Ajax傳入的參數
-	 
-	        String userName = request.getParameter("userName");
-	 
-	        String[] arrayUserInterest = request.getParameterValues("userInterest");
-	 
-	        //建構要回傳JSON物件
-	 
-	        HashMap userInfoMap = new HashMap();
-	 
-	        userInfoMap.put("userName", userName);
-	 
-	        
-	 
-	        ArrayList userInterestList = new ArrayList();
-	 
-	        userInterestList.addAll(Arrays.asList(arrayUserInterest));
-	 
-	        userInfoMap.put("userInterest", userInterestList);
-	 
-	        
-	 
-	        JSONObject responseJSONObject = new JSONObject(userInfoMap);
-	 
-	        
-	 
-	        PrintWriter out = response.getWriter();
-	 
-	        out.println(responseJSONObject);
-	 
-	    }
+//	            throws ServletException, IOException {
+//	 
+//	        response.setContentType("text/html;charset=UTF-8");
+//	 
+//	        //對Post中文參數進行解碼
+//	 
+//	        request.setCharacterEncoding("UTF-8");
+//	 
+//	        //取得Ajax傳入的參數
+//	 
+//	        String userName = request.getParameter("userName");
+//	 
+//	        String[] arrayUserInterest = request.getParameterValues("userInterest");
+//	 
+//	        //建構要回傳JSON物件
+//	 
+//	        HashMap userInfoMap = new HashMap();
+//	 
+//	        userInfoMap.put("userName", userName);
+//	 
+//	        
+//	 
+//	        ArrayList userInterestList = new ArrayList();
+//	 
+//	        userInterestList.addAll(Arrays.asList(arrayUserInterest));
+//	 
+//	        userInfoMap.put("userInterest", userInterestList);
+//	 
+//	        
+//	 
+//	        JSONObject responseJSONObject = new JSONObject(userInfoMap);
+//	 
+//	        
+//	 
+//	        PrintWriter out = response.getWriter();
+//	 
+//	        out.println(responseJSONObject);
+//	 
+//	    }
 	 
 	 
 	 
