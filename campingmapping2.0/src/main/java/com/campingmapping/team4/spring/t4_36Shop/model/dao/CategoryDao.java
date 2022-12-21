7b71a38d0c39d8bda3c3450f28020d4f8fd312df
ryan <tommy19961028@gmail.com>
2022-12-22 02:20:32 +0800

    20221222 alldao

--- campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/dao/CategoryDao.java
+++ campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/dao/CategoryDao.java
@@ -1,8 +1,6 @@
 package com.campingmapping.team4.spring.t4_36Shop.model.dao;
 
-import java.sql.Blob;
 import java.sql.SQLException;
-import java.util.Date;
 import java.util.List;
 
 import com.campingmapping.team4.spring.t4_36Shop.model.entity.Category;
@@ -15,8 +13,7 @@ public interface CategoryDao {
 	// 依Pd_id來刪除單筆記錄
 	Boolean deleteByPd_id(int id) throws SQLException;
 
-	Category update(Integer pdid, String userID, String pdname, String pdtitle, String pdcontent, String pdtype,
-			Blob pdpicture, int pdprice, int pdinventory, Date pdlastupdate) throws SQLException;
+	void update(Category category) throws SQLException;
 
 	Category selectByPdid(int Pdid) throws SQLException;
 
--- campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/dao/impl/CategoryDaoImpl.java
+++ campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/dao/impl/CategoryDaoImpl.java
@@ -24,19 +24,6 @@ public class CategoryDaoImpl implements CategoryDao {
 		session.save(category);
 		return category;
 	}
-//	@Override	
-//	public boolean insert(Category category) throws SQLException {
-//		String sql = "INSERT INTO " + "category"
-//				+ " ( userID, Pdname, Pdtitle, Pdcontent, Pdtype, Pdpicture, Pdprice, Pdinventory, Pddate, Pdlastupdate) "
-//				+ "values( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-//
-//		Category result = queryRunner.insert(DbUtils.getConnection(), sql, new BeanHandler<>(Category.class),
-//				category.getUserID(), category.getPdname(), category.getPdtitle(), category.getPdcontent(),
-//				category.getPdtype(), category.getPdpicture(), category.getPdprice(), category.getPdinventory(),
-//				category.getPddate(), category.getPdlastupdate());
-//
-//		return Objects.nonNull(result);
-//	}
 
 	// 依Pd_id來刪除單筆記錄
 	public Boolean deleteByPd_id(int id) {
@@ -49,46 +36,16 @@ public class CategoryDaoImpl implements CategoryDao {
 
 		return false;
 	}
-//	@Override
-//	public int deleteByPd_id(int id) throws SQLException {
-//		String sql = "DELETE FROM category WHERE Pdid = ?";
-//
-//		return queryRunner.update(DbUtils.getConnection(), sql, id);
-//	}
 
-	// 修改一筆書籍資料
-	public Category update(Integer pdid, String userID, String pdname, String pdtitle, String pdcontent, String pdtype,
-			Blob pdpicture, int pdprice, int pdinventory, Date pdlastupdate) {
-		Category category = session.get(Category.class, pdid);
+	// 修改一筆產品資料
+	public void update(Category category) {
+		Category updateByPdid = session.get(Category.class, category.getPdid());
 
-		if (category != null) {
-			category.setPdid(pdid);
-			category.setUserID(userID);
-//			category.setuserID(session.get(member.class, userID));
-			category.setPdname(pdname);
-			category.setPdtitle(pdtitle);
-			category.setPdcontent(pdcontent);
-			category.setPdtype(pdtype);
-			category.setPdpicture(pdpicture);
-			category.setPdprice(pdprice);
-			category.setPdinventory(pdinventory);
-			category.setPdlastupdate(pdlastupdate);
-
-			return category;
+		if (updateByPdid != null) {
+			session.saveOrUpdate(category);
 		}
-
-		return null;
 	}
 
-//	@Override
-//	public int update(Category category) throws SQLException {
-//		String sql = "UPDATE category SET userID=?, Pdname =?, Pdtitle =?, Pdcontent =?, Pdtype =?, "
-//				+ "Pdprice =?, Pdinventory =?, Pdlastupdate =? WHERE Pdid = ?";
-//
-//		return queryRunner.update(DbUtils.getConnection(), sql, category.getUserID(), category.getPdtitle(),
-//				category.getPdcontent(), category.getPdname(), category.getPdtype(), category.getPdprice(),
-//				category.getPdinventory(), category.getPdlastupdate(), category.getPdid());
-//	}
 	// 使用Pdid搜尋
 	public Category selectByPdid(int Pdid) {
 		Category category = session.get(Category.class, Pdid);
@@ -99,12 +56,6 @@ public class CategoryDaoImpl implements CategoryDao {
 
 		return null;
 	}
-//	@Override
-//	public Category selectByPdid(int id) throws SQLException {
-//		String sql = "SELECT Pdid,userID,Pdname,Pdtitle,Pdcontent,Pdtype,Pdprice,Pdinventory,Pddate,Pdlastupdate FROM category WHERE Pdid = ?";
-//
-//		return queryRunner.query(DbUtils.getConnection(), sql, new BeanHandler<Category>(Category.class), id);
-//	}
 
 	// 透過Pdid找圖片
 //	@Override
@@ -128,41 +79,5 @@ public class CategoryDaoImpl implements CategoryDao {
 
 		return resultList;
 	}
-//	@Override
-//	public List<Category> selectAll() {
-//
-//		List<Category> categoryList = new ArrayList<>();
-//
-//		String sql = "SELECT * FROM category";
-//
-//		try (Connection connection = DbUtils.getConnection();
-//				PreparedStatement ps = connection.prepareStatement(sql);) {
-//			try (ResultSet rs = ps.executeQuery();) {
-//				// 只要還有紀錄未取出，rs.next()會傳回true
-//				// 迴圈內將逐筆取出ResultSet內的紀錄
-//				while (rs.next()) {
-//					// 準備一個新的BookBean，將ResultSet內的一筆紀錄移植到BookBean內
-//					Category category = new Category();
-//					category.setPdid(rs.getInt("Pdid"));
-//					category.setUserID(rs.getString("userID"));
-//					category.setPdname(rs.getString("Pdname"));
-//					category.setPdtitle(rs.getString("Pdtitle"));
-//					category.setPdcontent(rs.getString("Pdcontent"));
-//					category.setPdtype(rs.getString("Pdtype"));
-//					category.setPdpicture(rs.getBlob("Pdpicture"));
-//					category.setPdprice(rs.getInt("Pdprice"));
-//					category.setPdinventory(rs.getInt("Pdinventory"));
-//					category.setPddate(rs.getDate("Pddate"));
-//					category.setPdlastupdate(rs.getDate("Pdlastupdate"));
-//					// 最後將BookBean物件放入大的容器內
-//					categoryList.add(category);
-//				}
-//			}
-//		} catch (SQLException ex) {
-//			ex.printStackTrace();
-//		}
-//
-//		return categoryList;
-//	}
 
 }
