a5972d22673683cec58edd7a5af922cba93a1b3d
shihshihder <suc12345@gmail.com>
2022-12-22 12:14:29 +0800

    20221222webapp update

--- campingmapping/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/dao/impl/Ccccdao.java
+++
@@ -1,7 +0,0 @@
-package com.campingmapping.team4.spring.t4_01Member.model.dao.impl;
-
-import com.campingmapping.team4.spring.t4_01Member.model.entity.Cccc;
-
-public class Ccccdao extends BaseDaoImpl<Cccc>{
-
-}
