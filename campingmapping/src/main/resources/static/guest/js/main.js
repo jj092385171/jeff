58f1e7e79c46c14e88fda67dee7305e572b076a5
shihshihder <suc12345@gmail.com>
2022-12-29 18:58:48 +0800

    回家

--- campingmapping/src/main/resources/static/guest/js/main.js
+++ campingmapping/src/main/resources/static/guest/js/main.js
@@ -1,62 +1,19 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
     
-    
-    // Initiate the wowjs
+    // Initiate the wowjs animation library
     new WOW().init();
-
-
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
-        } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
-        }
+    
+    // Initiate menu
+    $('#header').after('<div class="mobile-menu d-xl-none">');
+    $('.top-menu').clone().appendTo('.mobile-menu');
+    $('.mobile-menu-btn').click(function () {
+        $('.mobile-menu').stop().slideToggle();
     });
-    
-    
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-    
     
     // Back to top button
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
+        if ($(this).scrollTop() > 100) {
             $('.back-to-top').fadeIn('slow');
         } else {
             $('.back-to-top').fadeOut('slow');
@@ -66,29 +23,46 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
+    
+    //Portfolio modal slider
+    $('.port-slider').delay(10000);
+    $('.port-slider').slick({
         autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.port-slider-nav'
+    });
+    $('.port-slider-nav').slick({
+        autoplay: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.port-slider',
+        arrows: false,
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
     });
     
+    $('#popover-content-download').hide();
+    $("[data-toggle=popover]").each(function (e) {
+        $(this).popover({
+            html: true,
+            content: function () {
+                var id = $(this).attr('id')
+                return $('#popover-content-' + id).html();
+            }
+        });
+
+    });
+    
+    // Date and time picker
+    $('#date-1, #date-2, #date-3, #date-4, #date-5, #date-6').datetimepicker({
+        format: 'L'
+    });
+    $('#time-1, #time-2').datetimepicker({
+        format: 'LT'
+    });
 })(jQuery);
 
