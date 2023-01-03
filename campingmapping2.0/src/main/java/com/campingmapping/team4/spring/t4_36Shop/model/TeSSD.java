8e7a854976981049ca96d7685e967229acb3f3b4
ryan <tommy19961028@gmail.com>
2023-01-03 17:49:51 +0800

    20230103

--- campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/TeSSD.java
+++ campingmapping2.0/src/main/java/com/campingmapping/team4/spring/t4_36Shop/model/TeSSD.java
@@ -3,7 +3,8 @@ package com.campingmapping.team4.spring.t4_36Shop.model;
 public class TeSSD {
 
 	public static void main(String[] args) {
-System.out.println(1233);
+		System.out.println(1233);
+		//asdasdasd
 	}
 
 }
