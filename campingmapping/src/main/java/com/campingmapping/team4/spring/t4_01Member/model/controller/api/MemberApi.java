436d4122a44ca5cec6350fd0be818bdb9fe022de
shihshihder <suc12345@gmail.com>
2023-01-04 16:45:22 +0800

    404設置

--- campingmapping/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/controller/api/MemberApi.java
+++ campingmapping/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/controller/api/MemberApi.java
@@ -2,16 +2,12 @@ package com.campingmapping.team4.spring.t4_01Member.model.controller.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.campingmapping.team4.spring.t4_01Member.model.entity.Member;
 import com.campingmapping.team4.spring.t4_01Member.model.service.impl.MemberServiceImpl;
-
-import lombok.extern.apachecommons.CommonsLog;
 
 @Controller
 @RequestMapping("memberapi")
@@ -31,26 +27,6 @@ public class MemberApi {
 	public Member getMemberById() {
 
 		return memberServiceImpl.findMemberById(1);
-	}
-
-	@GetMapping(path = "/class2", produces = "text/plain;charset=UTF-8")
-	@ResponseBody
-	public String processResponseAction2() {
-		return "hi, how are u? 你好好";
-	}
-
-	@GetMapping(path = "/class1")
-	@ResponseBody
-	public ResponseEntity<String> class1() {
-
-		return new ResponseEntity<String>("custom status code(403 forbidden)", HttpStatus.FORBIDDEN);
-	}
-
-	@GetMapping(path = "/class3")
-	@ResponseBody
-	public ResponseEntity<String> class13() {
-
-		return new ResponseEntity<String>("custom status code(403 forbidden)", HttpStatus.FORBIDDEN);
 	}
 
 }
