4b5d9ecee4fc5d9db464b734407fe21db9170b18
shihshihder <suc12345@gmail.com>
2023-01-05 15:50:19 +0800

    測試API

---
+++ campSQL/01/boot/t4_33Forum/model/entity/VoteOption.java
@@ -0,0 +1,30 @@
+// package com.campingmapping.team4.spring.t4_33Forum.model.entity;
+
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToOne;
+// import jakarta.persistence.Table;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Data
+// @NoArgsConstructor
+// @Entity
+// @Table(name = "VoteOption")
+// public class VoteOption {
+
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// @Column(name = "optionId")
+// private Integer optionId;
+// @Column(name = "optionName")
+// private String optionName;
+// @ManyToOne
+// @JoinColumn(name = "voteId")
+// private Integer voteId;
+
+// }
---
+++ campingmapping/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/entity/Profiles.java
@@ -0,0 +1,43 @@
+package com.campingmapping.team4.spring.t4_01Member.model.entity;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "profiles")
+@Component
+public class Profiles {
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "NAME")
+	private String name;
+
+	@Column(name = "ADDRESS")
+	private String address;
+
+	@Column(name = "PHONE")
+	private String phone;
+
+	public Profiles(Integer id, String name, String address, String phone) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+
+}
