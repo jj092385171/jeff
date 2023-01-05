f81b1ded045b769b771916f47e3fdc2b4c38e7c1
shihshihder <suc12345@gmail.com>
2023-01-05 12:05:42 +0800

    yml update

--- campingmapping/src/main/java/com/campingmapping/team4/spring/utils/security/WebSecurityConfig.java
+++ campingmapping/src/main/java/com/campingmapping/team4/spring/utils/security/WebSecurityConfig.java
@@ -1,10 +1,18 @@
-package com.campingmapping.team4.spring.utils.security;
+// package com.campingmapping.team4.spring.utils.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import
+// org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
+// @Configuration
+// public class WebSecurityConfig extends WebSecurityConfiguration{
 
-}
+// @Bean
+// public PasswordEncoder getPasswordEncoder(){
+// return new BCryptPasswordEncoder();
+// }
+
+// }
