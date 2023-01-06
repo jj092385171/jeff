95a592deb237fe422fb140826e7294c75998e5fd
kevin <hcc8462@gmail.com>
2023-01-06 17:25:39 +0800

    1/6正在改insert

--- campingmapping3.0/src/main/java/com/campingmapping/team4/spring/t4_09Job/controller/JobWorkController.java
+++ campingmapping3.0/src/main/java/com/campingmapping/team4/spring/t4_09Job/controller/JobWorkController.java
@@ -1,11 +1,13 @@
 package com.campingmapping.team4.spring.t4_09Job.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.campingmapping.team4.spring.t4_09Job.model.entity.JobWorkBean;
 import com.campingmapping.team4.spring.t4_09Job.model.service.JobWorkService;
@@ -17,9 +19,22 @@ public class JobWorkController {
 	private JobWorkService jobWorkService;
 	
 	@PostMapping(path = "/JobServletAdd")
-	public String insert(@ModelAttribute("members") JobWorkBean mem2, BindingResult result, Model m2) {
-		if (result.hasErrors()) {
-			return "membersError";
+	public String insert(@RequestParam("uid") String uid,
+			@RequestParam("job") String job,@RequestParam("salary") String salary,
+			@RequestParam("quantity") String quantity,@RequestParam("place")String place,
+			@RequestParam("time") String time,@RequestParam("date") String date,
+			@RequestParam("rackUp") String rackUp,@RequestParam("rackDown") String rackDown, 
+			@RequestParam("img") String img,@RequestParam("remark") String remark,Model m) {
+		
+		Map<String, String> errorMessage = new HashMap<>();
+		m.addAttribute("errors", errorMessage);
+		// 驗證會員id輸入格式
+				try {
+					Integer uID = Integer.parseInt(uid);
+				} catch (Exception e) {
+					errorMessage.put("id", "輸入格式錯誤");
+				}
+		return "";
 		}
-	
 }
+
