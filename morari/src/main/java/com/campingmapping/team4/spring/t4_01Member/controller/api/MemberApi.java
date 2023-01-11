1300e01e0966605479a1b504baf4c6b2fc625cc5
一日一傳 <>
2023-01-11 21:01:47 +0800

    CSRF

---
+++ morari/src/main/java/com/campingmapping/team4/spring/t4_01Member/controller/api/MemberApi.java
@@ -0,0 +1,32 @@
+// package com.campingmapping.team4.spring.t4_01Member.controller.api;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.jpa.repository.EntityGraph;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+// import com.campingmapping.team4.spring.t4_01Member.model.entity.Member;
+// imp // com.campingmapping.team4.spring.t4_01Member.model.service.impl.MemberServiceImpl;
+
+// @Controller
+// @RequestMapping("memberapi")
+// public class MemberAp
+    // @Autowi
+    // MemberServiceImpl memberServiceIm
+
+    // @GetMapping(path = "/showal
+    // @ResponseB
+    // public Iterable<Member> getMemberList(
+        // return memberServiceImpl.showMember
+    /
+
+    // @GetMapping("/membe
+    // @ResponseB
+    // @EntityGraph(attributePaths = { "Member"
+    // public Member getMemberById(
+
+        // return memberServiceImpl.findMemberById(
+    // }
+
+// }
