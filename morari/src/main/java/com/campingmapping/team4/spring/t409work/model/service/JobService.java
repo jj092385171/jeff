47841fa6c58c60d3c642a3db2ab323e4ec2f3757
jack <superhuang840325@yahoo.com.tw>
2023-01-12 19:02:13 +0800

    跑動

---
+++ morari/src/main/java/com/campingmapping/team4/spring/t409work/model/service/JobService.java
@@ -0,0 +1,41 @@
+package com.campingmapping.team4.spring.t409work.model.service;
+
+import java.io.InputStream;
+import java.sql.Blob;
+import java.util.List;
+
+import com.campingmapping.team4.spring.t409work.model.entity.JobBean;
+
+public interface JobService {
+
+	// 秀全部
+	public List<JobBean> showAllJob();
+
+	// 查職缺
+	public List<JobBean> findJobByJobLike(String job);
+
+	// 新增職缺
+	public void addJob(JobBean jobBean);
+
+	// 上傳圖片
+	public Blob fileToBlob(InputStream is, long size);
+
+	// 透過rackID秀圖片
+	public JobBean findImgByRackID(int rackID);
+
+	// 透過rackID刪除資料
+	public void deleteJob(int rackID);
+
+	// 改職缺內容
+	public void updateJob(JobBean jobBean);
+
+	// 透過rackID抓一筆資料
+	public JobBean findBeanByRackID(int rackID);
+
+	// 透過會員id找資料
+	public List<JobBean> findBeanByuID(int uID);
+
+	// 判斷時間
+	// public boolean selectTime(Date rackUp, Date rackDown);
+
+}
