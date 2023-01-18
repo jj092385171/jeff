321b872c3e4b2915a9303558ac1c194ebe84b1ce
kevin <hcc8462@gmail.com>
2023-01-18 13:51:29 +0800

    Merge branch '36' of https://github.com/ShihShihDer/EEIT56_Team4-CampingMapping- into test
    # Conflicts:
    #	DemoSpringBootProject3/.mvn/wrapper/maven-wrapper.properties
    #	DemoSpringBootProject3/mvnw
    #	DemoSpringBootProject3/mvnw.cmd
    #	DemoSpringBootProject3/pom.xml
    #	DemoSpringBootProject3/src/test/java/tw/spring/DemoSpringBootProject3ApplicationTests.java
    #	campingmapping/src/main/resources/static/admin/vendor/fontawesome-free/css/fontawesome.min.css
    #	campingmapping2.0/src/main/webapp/T4_11/background/assets/css/fontawesome-all.min.css
    #	campingmapping2.0/src/main/webapp/T4_11/jquery-ui-1.13.2.custom/external/jquery/jquery.js
    #	campingmapping3.0/pom.xml
    #	campingmapping3.0/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/service/JoinService.java
    #	campingmapping3.0/src/main/java/com/campingmapping/team4/spring/t4_01Member/model/service/ToolService.java
    #	campingmapping3.0/src/main/java/com/campingmapping/team4/spring/t4_11Team/servlet/ViewController.java
    #	campingmapping3.0/src/main/webapp/WEB-INF/pages/t4_01member/login.html
    #	campingmapping3.0/src/main/webapp/WEB-INF/pages/t4_09job/job/JobModel/assets/css/fontawesome-all.min.css
    #	morari/src/main/java/com/campingmapping/team4/spring/t401member/model/entity/UserProfiles.java
    #	morari/src/main/resources/application.properties
    #	morari/src/main/resources/application.yml
    #	morari/src/main/resources/templates/index.html
    #	morari/src/main/resources/templates/mall/guest/mallallproduct.html
    #	morari/src/main/resources/templates/work/guest/index.html
    #	securoty/spring-boot-3-jwt-security-main/src/main/resources/static/favicon.ico

--- morari/src/main/java/com/campingmapping/team4/spring/t409work/controller/web/WorkPageController.java
+++ morari/src/main/java/com/campingmapping/team4/spring/t409work/controller/web/WorkPageController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -105,10 +106,10 @@ public class WorkPageController {
 	}
 
 	// 刪除
-	@PostMapping("/jobDelete.controller")
-	public boolean processDeleteAction(@RequestParam("de") Integer rackID) {
+	@DeleteMapping("/jobDelete.controller/{de}")
+	public String processDeleteAction(@PathVariable("de") Integer rackID) {
 		jService.deleteById(rackID);
-		return true;
+		return "ok";
 	}
 
 	// 模糊搜尋
