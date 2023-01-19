3877b0e9962bacdceb7f1b1b98ba0d1bd9a6e88c
jack <superhuang840325@yahoo.com.tw>
2023-01-19 22:59:28 +0800

    35465

--- morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/siteCrud/UpdateSiteByIDController.java
+++ morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/siteCrud/UpdateSiteByIDController.java
@@ -10,24 +10,28 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.campingmapping.team4.spring.t424camp.model.entity.Site;
 import com.campingmapping.team4.spring.t424camp.model.service.SiteService;
 
 @Controller
+@RequestMapping("/admin/camp")
 public class UpdateSiteByIDController {
 
 	@Autowired
 	private SiteService siteService;
 
-	@PostMapping("/sitesOfCamp.controller/updateSiteByID.controller")
-	public String updateSiteByID(@RequestParam("siteID") Integer siteID,
+	@PostMapping("/updateSiteByID.controller")
+	@ResponseBody
+	public Site updateSiteByID(@RequestParam("siteID") Integer siteID,
 			@RequestParam("siteName") @Nullable String siteName,
 			@RequestParam("sitePicturesPath") @Nullable MultipartFile mf,
-			@RequestParam("totalSites") @Nullable String totalSites,
-			@RequestParam("siteMoney") @Nullable String siteMoney, @RequestParam("campID") Integer campID, Model m)
+			@RequestParam("totalSites") @Nullable Integer totalSites,
+			@RequestParam("siteMoney") @Nullable Integer siteMoney, @RequestParam("campID") Integer campID, Model m)
 			throws IllegalStateException, IOException {
 
 		// 存錯誤的map
@@ -49,30 +53,29 @@ public class UpdateSiteByIDController {
 		mf.transferTo(saveFilePath);
 
 		// 總營位數
-		if (totalSites == null || totalSites.length() == 0) {
+		if (totalSites == null ) {
 			errors.put("totalSites", "必須輸入總營位數");
 		}
 
 		// 營位金額
-		if (siteMoney == null || siteMoney.trim().length() == 0) {
+		if (siteMoney == null ) {
 			errors.put("siteMoney", "必須輸入營位金額");
 		}
 
-		// 錯誤導回
-		if (errors != null && !errors.isEmpty()) {
-			m.addAttribute("campID", campID);
-			m.addAttribute("errors", errors);
+//		// 錯誤導回
+//		if (errors != null && !errors.isEmpty()) {
+//			m.addAttribute("campID", campID);
+//			m.addAttribute("errors", errors);
+//
+//			return "t4_24camp/admin/UpdateSiteByIDForm";
+//		}
 
-			return "t4_24camp/admin/UpdateSiteByIDForm";
-		}
+		Site site = siteService.update(siteID, siteName, fileName, totalSites, siteMoney, campID);
 
-		Site site = siteService.update(siteID, siteName, fileName, Integer.valueOf(totalSites),
-				Integer.valueOf(siteMoney), campID);
+//		m.addAttribute("site", site);
+//		m.addAttribute("what", "更新");
 
-		m.addAttribute("site", site);
-		m.addAttribute("what", "更新");
-
-		return "t4_24camp/admin/InsertUpdateSiteSuccess";
+		return site;
 	}
 
 }
