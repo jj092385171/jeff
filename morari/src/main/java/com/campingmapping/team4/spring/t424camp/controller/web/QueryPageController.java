9b17ea3262a813878e9b4357be3853b1daf95442
jack <superhuang840325@yahoo.com.tw>
2023-01-19 10:14:44 +0800

    21345

--- morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/QueryPageController.java
+++ morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/QueryPageController.java
@@ -1,16 +1,16 @@
-package com.campingmapping.team4.spring.t424camp.controller.web;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/admin/camp")
-public class QueryPageController {
-
-	@GetMapping("/queryPage.controller")
-	public String toPage() {
-		return "camp/admin/QueryPageForm";
-	}
-
-}
+//package com.campingmapping.team4.spring.t424camp.controller.web;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/admin/camp")
+//public class QueryPageController {
+//
+//	@GetMapping("/queryPage.controller")
+//	public String toPage() {
+//		return "camp/admin/QueryPageForm";
+//	}
+//
+//}
--- morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/campCrud/QueryCampsController.java
+++ morari/src/main/java/com/campingmapping/team4/spring/t424camp/controller/web/campCrud/QueryCampsController.java
@@ -6,11 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.campingmapping.team4.spring.t424camp.model.entity.Camp;
@@ -26,11 +24,6 @@ public class QueryCampsController {
 	@Autowired
 	private CampService campService;
 
-	
-	@GetMapping("/QueryByCityIDsResult")
-	public String queryByCityIDsResult() {
-		return "camp/admin/QueryByCityIDsResult";
-	}
 	
 	@PostMapping("/queryCampsByCityIDs.controller")
 	@ResponseBody
@@ -55,32 +48,28 @@ public class QueryCampsController {
 
 		return camps;
 	}
-
-	@GetMapping("/QueryByIDResult")
-	public String queryByIDResult() {
-		return "camp/admin/QueryByIDResult";
-	}
 	
-	@PostMapping("/queryCampsByCampID.controller")
-	public String queryByID(@RequestParam("campID") String campID, Model m) {
+	@PostMapping("/queryCampByID.controller")
+	@ResponseBody
+	public Camp queryByID(@RequestBody Integer campID, Model m) {
 
 		HashMap<String, String> errors = new HashMap<String, String>();
 		m.addAttribute("errors", errors);
 
 		// 縣市
-		if (campID == null || campID.length() == 0) {
-			errors.put("cityID", "必須勾選縣市");
+		if (campID == null) {
+			errors.put("cityID", "必須輸入ID");
 		}
 
-		// 錯誤導回
-		if (errors != null && !errors.isEmpty()) {
-			return "redirect:/t4_24camp/admin/QueryPageForm";
-		}
+//		// 錯誤導回
+//		if (errors != null && !errors.isEmpty()) {
+//			return "redirect:/t4_24camp/admin/QueryPageForm";
+//		}
 
-		Camp camp = campService.findById(Integer.valueOf(campID));
-		m.addAttribute("camp", camp);
+		Camp camp = campService.findById(campID);
+//		m.addAttribute("camp", camp);
 
-		return "t4_24camp/admin/QueryByCampIDResult";
+		return camp;
 	}
 
 }
