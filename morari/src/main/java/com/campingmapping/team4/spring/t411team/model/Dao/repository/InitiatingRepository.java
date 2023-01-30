05b64e53bbcc2ff1f5c9ab00b06535f85a5cd7b9
Jeff <rujeff62405@gmail.com>
2023-01-30 19:11:30 +0800

    新增功能：動態查詢

--- morari/src/main/java/com/campingmapping/team4/spring/t411team/model/Dao/repository/InitiatingRepository.java
+++ morari/src/main/java/com/campingmapping/team4/spring/t411team/model/Dao/repository/InitiatingRepository.java
@@ -4,17 +4,19 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import com.campingmapping.team4.spring.t401member.model.entity.UserProfiles;
 import com.campingmapping.team4.spring.t411team.model.entity.Initiating;
 
 public interface InitiatingRepository extends JpaRepository<Initiating, Integer> {
+	
+	public List<Initiating> findByStartdateGreaterThanEqual(Date startdate);
+	
+	public List<Initiating> findByEnddateLessThanEqual(Date enddate);
 	
 	public List<Initiating> findByStartdateGreaterThanEqualAndEnddateLessThanEqual(Date startdate, Date enddate);
 	
 	public List<Initiating> findByCamparea(String camparea);
 	
-	@Query(value = "select * from where uid = :uid",nativeQuery = true)
-	public List<Initiating> findByUid(@Param("uid") String uid);
+	public List<Initiating> findByUserprofiles(UserProfiles uid);
 }
