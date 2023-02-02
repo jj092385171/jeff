d5ad0f1966919577d16eeba06d7e28ba803ab801
Jeff <rujeff62405@gmail.com>
2023-02-02 12:37:59 +0800

    2023/02/02

--- morari/src/main/java/com/campingmapping/team4/spring/CampingmappingApplication.java
+++ morari/src/main/java/com/campingmapping/team4/spring/CampingmappingApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CampingmappingApplication {
 
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(CampingmappingApplication.class, args);
 	}
