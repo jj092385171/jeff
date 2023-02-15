e3add32225c116616a8835d0e92a3fa5d06300cc
eyes <z98765432123456789@yahoo.com.tw>
2023-02-15 19:34:28 +0800

    可以連到camper

--- morari/src/main/resources/static/camper/js/guestpage.js
+++ morari/src/main/resources/static/camper/js/guestpage.js
@@ -17,8 +17,31 @@ fetch("/morari/camper/html/guestpage.html")
                 document.getElementById("campershot").src = data.shot;
                 document.getElementById("about").textContent = data.about;
                 
-                
-                
+                $.ajax({
+					url: "/morari/showpostbyuserid.controller/" + uid,
+					dataType: "JSON",
+					contentType: "application/json",
+					success: function (data) {
+						var div = "<div id='rooms'><div class='row'>";
+						$.each(data, function (i, n) {
+							// i=index 第幾個 n=element 元素
+							div += "<div class='col-md-12 room'><div class='row'>";
+							div += "<div class='col-md-6'><div class='room-img'>";
+							if(n.picture){
+								div += "<img src='"+ n.picture +"' width='100%'>";
+							}else{
+								div += "<img src='/morari/forum/img/nopicture.jpeg' width='100%' height='350px'>";
+							}
+							div += "</div></div>";
+							div += "<div class='col-md-6'><div class='room-des'>";
+							div += "<h1>" + n.title + "</h1>" + n.content + "<br><br>更新時間：" + n.releasedate + "<div class='room-link'><a href='/morari/forum/showpost.controller/" + n.postid + "'>查看貼文</a></div>";
+							div += "</div></div>";
+							div += "</div></div>";
+						});
+						div += "</div></div>";
+						$("#camperpost").append(div);
+					}
+				});
                 
             })            
         } 
