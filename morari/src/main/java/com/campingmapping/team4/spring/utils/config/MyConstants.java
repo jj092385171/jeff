68f199c6b128c662541c4cc2c259500631a5ea67
kevin <hcc8462@gmail.com>
2023-02-22 14:22:29 +0800

    123

--- morari/src/main/java/com/campingmapping/team4/spring/utils/config/MyConstants.java
+++ morari/src/main/java/com/campingmapping/team4/spring/utils/config/MyConstants.java
@@ -5,7 +5,7 @@ public class MyConstants {
     public final static String JWT_COOKIE_NAME = "R9SBUCVDWF";//accesstoken
     public final static String JWT_REFRESH_COOKIE_NAME = "4M5N6Q8R9S";//refreshtoken
     public final static String GOOGLE_COOKIE_NAME = "JSESSIONID";//google
-    public final static String GOOGLE_BUCKETNAME = "morariphoto"; // google storage name
+    public final static String GOOGLE_BUCKETNAME = "eeit56-img"; // google storage name
     // public final static Long ACCESS_TOKEN_VALIDATION_SECOND = 60L * 60 * 1 * 1000; // 1 hr
     public final static Long ACCESS_TOKEN_VALIDATION_SECOND =  5L * 1000; // 5s
     public final static Long REFRESH_TOKEN_VALIDATION_SECOND = 60L * 60 * 24 * 1 * 1000; // 1 Day
